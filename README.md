c80e336a7b8fd3bb9ac76ef4c7aa0f667ca32fb5
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2017-12-26 22:24:45 +0100

    replaced eureka with consul

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## Description
 
 This repository contains a Spring Cloud Reference Architecture containing of the following microservices:
-- Service Registry
 - Spring Boot Admin
 - Microservice 1 (Client)
 - Microservice 2 (Contacted by Microservice 1)
@@ -12,10 +11,13 @@ This repository contains a Spring Cloud Reference Architecture containing of the
 
 ## How to get started?
 
-Just run all Spring Boot Applications.
+Download Consul from https://www.consul.io/downloads.html and start with 
+`./consul agent -data-dir=/tmp/consul -dev -bind=127.0.0.1`
 
+Then run all Spring Boot Applications.
 
 ## URLs
+- Consul: http://localhost:8500
 - Microservice 1: http://localhost:8080/ms1
 - Spring Boot Admin: http://localhost:8000
 - Zipkin Server: http://localhost:7000
--- ms2/src/main/java/com/example/ms2/HelloController.java
+++ ms2/src/main/java/com/example/ms2/HelloController.java
@@ -2,7 +2,6 @@ package com.example.ms2;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,17 +12,12 @@ public class HelloController {
 
     private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    private final EurekaInstanceConfigBean config;
-
-    public HelloController(EurekaInstanceConfigBean config) {
-        this.config = config;
-    }
 
     @GetMapping("/greet/{who}")
     @ResponseBody
     public String hello(@PathVariable("who") String who) {
         logger.info("Hello {}!", who);
-        return "Hello " + who + " Instance: " + config.getInstanceId();
+        return "Hello " + who + " Instance: " + this;
     }
 
 }
