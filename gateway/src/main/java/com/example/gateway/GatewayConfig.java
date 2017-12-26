92bdae27fa1be66e42712a903e1ffd45a6e06db9
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2017-12-26 22:27:40 +0100

    replaced zuul with spring cloud gateway

---
+++ gateway/src/main/java/com/example/gateway/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.example.gateway;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+//    @Bean
+//    public RouteLocator customRouteLocator() {
+//        return Routes.locator()
+//                .route("ms1")
+//                .predicate(path("/ms"))
+//                .uri("lb://microservice-1")
+//                .build();
+//    }
+
+}
